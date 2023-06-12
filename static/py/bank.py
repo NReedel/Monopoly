4ab7828a76d1a25f67438375cfff261b7766f94d
Alicyn <75287008+AlicynRK@users.noreply.github.com>
2023-06-11 20:05:34 -0400

    Expanded bank.py
    Added various methods and data for the bank class, as well as some documentation to describe the actions of each method.

--- static/py/bank.py
+++ static/py/bank.py
@@ -1,1 +1,51 @@
 # bank.py
+
+###
+# *Name: Alicyn Knapp
+# *Credit: PennWest Projects! (discord server)
+# *Purpose: Define Monopoly bank traits and actions
+###
+
+from deeds import *
+
+class Bank:
+    ### -- Constructor ###
+    def __init__(self):
+        ### -- Data -- ###
+        # bills = {'ones': 40, 'fives': 40, 'tens': 40, 'twenties': 50, 'fifties': 30, 'hundreds': 20, 'five_hundreds': 20}
+        houses = 32
+        hotels = 12
+
+    ### -- Methods -- ###
+    # salary_payout() : int
+    def salary_payout():
+        """ Returns the amount of money received when a player passes GO """
+        return 200
+
+    # bonus_payout()
+        """ Handles community chest and chance card payouts """
+    
+    # mortgage_payout(deed)
+        """ Returns the mortgage value of the given deed """
+
+    # conduct_auction()
+        """ Holds an auction between players for a deed """
+
+    # deed_purchase()
+        """ Handles the purchase of a deed between the deed price and player balance """
+
+    # house_purchase()
+        """ Handles the purchase of a house on a deed between the deed house price and the player balance """
+
+    # hotel_purchase()
+        """ Handles the purchase of a hotel on a deed between the deed hotel price and the player balance """
+        """     and checks that the deed has 4 houses before hotel purchase """
+    
+    # refund_deed()
+        
+
+    # tax_fines_collections()
+
+    # unmortgage_collections()
+
+    # bankrupt_balance_collections()
