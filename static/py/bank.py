0850dbe9ffdad44553d405e02cfda4f814825cea
Alicyn <75287008+AlicynRK@users.noreply.github.com>
2023-06-17 23:52:22 -0400

    Added Transactional Methods
    Card Number: 7
    
    Added two purchase event/transactional methods. See the below UML statements for usage info:
    + pay_bank(self, current_balance : int, amount_paid : int) : int
    + receive_bank(self, current_balance : int, amount_received : int) : int
    
    pay_bank is used as though another source were paying the bank; it returns the current balance of the source minus the amount paid by the source.
    receive_bank is used as though another source were receiving money from the bank; it returns the current balance of the source plus the amount paid by the bank.

--- static/py/bank.py
+++ static/py/bank.py
@@ -1,10 +1,10 @@
 # bank.py
 
-###
-# *Name: Alicyn Knapp
-# *Credit: PennWest Projects! (discord server)
-# *Purpose: Define Monopoly bank traits and actions
-###
+'''
+    *Name: Alicyn Knapp
+    *Credit: PennWest Projects! (discord server)
+    *Purpose: Define Monopoly bank traits and actions
+'''
 
 from deeds import *
 
@@ -17,35 +17,48 @@ class Bank:
         hotels = 12
 
     ### -- Methods -- ###
-    # salary_payout() : int
+    def pay_bank(self, current_balance = int(0), amount_paid = int(0)):
+        """ Subtracts amount paid to bank from current balance and returns the new balance """
+        # print('Paid $', amount_paid, ' to The Bank.')
+        return current_balance - amount_paid
+
+    def receive_bank(self, current_balance = int(0), amount_received = int(0)):
+        """ Adds amount received from bank to current balance and returns the new balance """
+        # print('Received $', amount_received, ' from The Bank.')
+        return amount_received + current_balance
+
+    '''
     def salary_payout():
         """ Returns the amount of money received when a player passes GO """
         return 200
+    '''
 
     # bonus_payout()
-        """ Handles community chest and chance card payouts """
+    """ Returns community chest and chance card payouts """
     
     # mortgage_payout(deed)
-        """ Returns the mortgage value of the given deed """
+    """ Returns the mortgage value of the given deed """
 
     # conduct_auction()
-        """ Holds an auction between players for a deed """
+    """ Holds an auction between players for a deed """
 
     # deed_purchase()
-        """ Handles the purchase of a deed between the deed price and player balance """
+    """ Handles the purchase of a deed between the deed price and player balance """
 
     # house_purchase()
-        """ Handles the purchase of a house on a deed between the deed house price and the player balance """
+    """ Handles the purchase of a house on a deed between the deed house price and the player balance """
 
     # hotel_purchase()
-        """ Handles the purchase of a hotel on a deed between the deed hotel price and the player balance """
-        """     and checks that the deed has 4 houses before hotel purchase """
-    
-    # refund_deed()
+    """ Handles the purchase of a hotel on a deed between the deed hotel price and the player balance """
+    """     and checks that the deed has 4 houses before hotel purchase """
         
-
     # tax_fines_collections()
+    """ Collects taxes from special tiles and fines from Community Chest/Chance """
 
     # unmortgage_collections()
+    """ Collects unmortgage deed funds from player """
 
     # bankrupt_balance_collections()
+    """ Collects deeds, money, houses/hotels, and Get Out Of Jail Free card(s) from player. """
+    """ Auctions deeds and conducts most if not all post-bankruptcy management """
+
