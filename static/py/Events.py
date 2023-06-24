6c28f56240c8717eecf660ce7b1fa8664915270c
NReedel <125296416+NReedel@users.noreply.github.com>
2023-06-24 14:31:52 -0400

    Add files via upload

---
+++ static/py/Events.py
@@ -0,0 +1,37 @@
+###
+# *Name:      Nate Reedel
+# *Credit:    PennWest Projects! (discord server)
+# *Purpose:   Handle specialised user event and display event options
+###
+
+###############################################################
+# Events Hierarchy 🔳
+###############################################################
+'''
+Events
+├── PlayerEvents
+├── JailedPlayerEvents
+├── MenuePlayerEvents   
+├── BankruptPlayerEvents
+├── AvaliablePropertyEvents
+├── TakenPropertyEvents
+├── AuctionPropertyEvents    
+├── MainMenueEvents
+###maybe###
+├── SellPropertyEvents
+├── BuildPropertyEvents         
+├── MortgagePropertyEvents
+├── RedeemPropertyEvents         
+├── TradeEvents
+'''
+###############################################################
+
+class Events:
+   #--Global Data--
+   events = [] # event : list<str>
+   
+   #--Method Implementation--
+   def display_event_options(): # display_event_options() : void
+      return 
+   def event(): # event() : void
+      return
