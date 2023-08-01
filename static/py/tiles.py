71d755be08d6b9e1c46a5e9936a5fa52069bc0d5
NReedel <125296416+NReedel@users.noreply.github.com>
2023-07-31 21:18:27 -0400

    Update tiles.py: now usable in board class
    Takes tile as argument as appose to index for all constructors.
    tile_index and tile_name is assigned at tile construction.
    Now references owners via owned_by string.
    TileSpecial classes now have special_tile data.

--- static/py/tiles.py
+++ static/py/tiles.py
@@ -42,10 +42,10 @@ Tile
 
 class Tile:
 	
-    def __init__ (self, index):
+    def __init__ (self, tile):
 
-        self.tile_id = index
-        self.tile_name = ""
+        self.tile_id = tile['index']
+        self.tile_name = tile['name']
         
         self.event_on_land = None   # ⚠⚠⚠
         self.occupants = []         # list of player ids
@@ -63,65 +63,75 @@ class Tile:
 
 class TileProperty(Tile):
 
-    def __init__ (self):
+    def __init__ (self, tile):
 
-        super(TileProperty, self).__init__(index)
+        super().__init__(tile)
 
         self.property_cost = 0
         self.mortgage_value = 0
         self.is_mortgaged = False
         self.rent_base = 0
 
-        self.owned_by = None    # player id
+        self.owned_by = "bank"    # player id, "bank", "none"
         self.set_num_owned = 0
         self.set_num_total = 0
+        
+        
+
+        # note: bank class needs identifier for owner to work 
+        self.houses = int(0)
+        self.hotels = int(0)
+        self.tile_type = tile['type']  # "street", "railroad", "utility"
         self.monopoly_type = Monopoly.NONE
 
 
 class PropertyStreet(TileProperty):
 
-    def __init__ (self):
+    def __init__ (self,tile):
     
-        super(PropertyStreet, self).__init__(index)
-        
+        super(PropertyStreet, self).__init__(tile)
         self.tile_color = ""    # need to standardize color format, ie. hex
-
         self.cost_house = 0
         self.num_houses = 0
         self.num_hotels = 0
 
-    def can_buy_hotel():
-        return self.num_houses == 4
+        
+        
+    # def can_buy_hotel():
+    #     return self.num_houses == 4
 
 
-class PropertySpecial(Tile):
+class PropertySpecial(TileProperty):
 
-    def __init__ (self):
-        super(PropertySpecial, self).__init__(index)
+    def __init__ (self,tile):
+        super(PropertySpecial, self).__init__(tile)
         self.fname_icon = ""
+        
 
 
 class PropertyRailroad(PropertySpecial):
 
-    def __init__ (self):
-        super(PropertyRailroad, self).__init__(index)
+    def __init__ (self,tile):
+        super(PropertyRailroad, self).__init__(tile)
 
-    def get_rent_owed():
-        return self.set_num_owned * 25
+        
+    # def get_rent_owed():
+    #     return self.set_num_owned * 25
 
 
 class PropertyUtility(PropertySpecial):
 
-    def __init__ (self):
-        super(PropertyUtility, self).__init__(index)
+    def __init__ (self,tile):
+        super(PropertyUtility, self).__init__(tile)
 
-    def get_rent_owed(roll):
-        if self.set_num_owned == 2:
-            return roll * 10
-        elif self.set_num_owned == 1:
-            return roll * 4
-        else:
-            return 0
+
+    # def get_rent_owed(roll):
+    #     if self.set_num_owned == 2:
+    #         return roll * 10
+    #     elif self.set_num_owned == 1:
+    #         return roll * 4
+    #     else:
+    #         return 0
 
 
 ###############################################################
@@ -129,28 +139,29 @@ class PropertyUtility(PropertySpecial):
 ###############################################################
 
 class TileSpecial(Tile):
-    def __init__ (self):
-        super(TileSpecial, self).__init__(index)
+    def __init__ (self,tile):
+        super(TileSpecial, self).__init__(tile)
         self.fname_icon = ""
-
+        self.owned_by = "none"
+        self.special_tile = tile['special'] # "corner" or "card" or"tax"
 
 # Chance and Community Chest
 class TileCard(TileSpecial):
-    def __init__ (self):
-        super(TileCard, self).__init__(index)
+    def __init__ (self,tile):
+        super(TileCard, self).__init__(tile)
 
 
 # Income and Luxury Tax
 class TileTax(TileSpecial):
-    def __init__ (self):
-        super(TileTax, self).__init__(index)
+    def __init__ (self,tile):
+        super(TileTax, self).__init__(tile)
         self.tax_amount = 0
 
 
 # Will need updating later ⚠⚠⚠
 class TileCorner(TileSpecial):
-    def __init__ (self):
-        super(TileCorner, self).__init__(index)
+    def __init__ (self,tile):
+        super(TileCorner, self).__init__(tile)
 
 
 ###############################################################
