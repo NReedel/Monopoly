f9dca4c4b9a1aa546af377b787692630c188de5f
NReedel <125296416+NReedel@users.noreply.github.com>
2023-08-06 14:33:34 -0400

    Update players.py
    added import copy
    changed player_number : int to id : str (match bank.id)
    removed player_number method
    added add_property and remove_property methods (still editing)

--- static/py/players.py
+++ static/py/players.py
@@ -1,4 +1,4 @@
-# players.py
+ # players.py
 
 ###
 # *Name:      Nate Reedel
@@ -6,24 +6,28 @@
 # *Purpose:   Define monopoly player traits and actions
 ###
 
+
+from deeds import *
+import copy
+
 class Players:
    
    # --Global Data--
    # nickname : string # represents username of player in monopoly
    # character : string # represents the figure a player uses in game
-   __player_number = int(0) # -__player_number : int
+   id = str(0) # -id : int
    __money = int(0) # -__money : int
    __location = int(0) # -__location : int
-   location_name = "Go" # location : str
+   location_name = "Go" # location 
    has_rolled = False
    same_values_rolled = int(0) # doublesrolled : int
 
    in_jail = False  # +in_Jail : bool 
    time_jailed = int(0) # +time_jailded : int 
    jail_free_card = int(0) # +jail_free_card : int
-   # owned_deeds = ["Reading Railroad","Baltic Ave","Boardwalk"] # 
-   owned_deeds = [] # +owned_deeds : list <Deeds>
-   owned_mortgages = [] # +owned_mortgages : list <Deeds> #maybe?
+   # deeds = ["Reading Railroad","Baltic Ave","Boardwalk"] # 
+   deeds = [] # +deeds : list <Deeds>
+   # owned_mortgages = [] # +owned_mortgages : list <Deeds> #maybe?
    total_houses = int(0) # +total_houses : int
    total_buildings = int(0) # +total_buildings : int
    bankrupt = False # +bankrupt : bool
@@ -31,13 +35,13 @@ class Players:
    #--Contstructor--
    def __init__(self,startingTotal = 0, id_number = 0):
       self.__money = startingTotal
-      self.__player_number = id_number
+      self.id = id_number
       
    #--Method Implementations--
    # player_assets(self) : void
    def player_status(self): 
       ###List Player Assets (static stats)
-      # print("player number = "self.__player_number)
+      # print("player number = "self.id)
       # print("player character = "self.character)
       print("\t\tmoney =","$"+str(self.current_money()))
       print("\t\tin debt =",self.in_debt())
@@ -49,9 +53,9 @@ class Players:
          print("\t\ttime jailed =",self.time_jailed)
       # if self.jail_free_card > 0:
       print("\t\tjail free cards =",self.jail_free_card)
-      print("\t\tproperties:",len(self.owned_deeds))
-      for i in range(0,len(self.owned_deeds)):
-         print("\t\t   -",self.owned_deeds[i]) #needs modification
+      print("\t\tproperties:",len(self.deeds))
+      for i in range(0,len(self.deeds)):
+         print("\t\t   -",self.deeds[i].name) #needs modification
       # print("\t\tmortgages :",len(self.owned_mortgages)) #maybe
       # for i in range(0,len(self.owned_mortgages)):
       #    print("\t\t   -",self.owned_mortgages[i])  #needs modification
@@ -61,16 +65,16 @@ class Players:
       # print("\t\t")
       print("\t\t-----------------------------") 
       
-   # player_number(self) : int
-   def player_number(self):
-      return int(self.__player_number)
+   # # player_number(self) : int
+   # def player_number(self):
+   #    return int(self.id)
    
    # move_location(next_location : int, location_text) : void
    def move_location(self,next_location, location_text): # new
       # print("move to location number",next_location)
       self.__location = next_location
       self.location_name = location_text
-      print("\n\t\tplayer",self.__player_number,"location is ","\n\t\t["+str(self.__location)+"]",self.location_name)
+      print("\n\t\tplayer",self.id,"location is ","\n\t\t["+str(self.__location)+"]",self.location_name)
       
    # current_location(self) : int
    def current_location(self):
@@ -83,12 +87,12 @@ class Players:
    # recieve_money(self, amount : int) : void
    def receive_money(self,amount):
       self.__money += amount
-      print("\t\tplayer",self.__player_number, "recieved $", amount)
+      print("\t\tplayer",self.id, "recieved $" + str(amount))
    
    # pay_money(self, amount : int ) :  int
    def pay_money(self,amount):
       self.__money -= amount
-      print("\t\tplayer",self.__player_number, "payed $", amount)
+      print("\t\tplayer",self.id, "payed $" + str(amount))
    
    # change_balance(self, new_total : int) : void
    def set_balance(self, new_total):
@@ -104,13 +108,30 @@ class Players:
    # go_to_Jail() : void
    def go_to_jail(self): 
       self.__location = 10
-      print("\t\tplayer", self.__player_number,"location =", self.__location) 
+      print("\t\tplayer", self.id,"location =", self.__location) 
       self.in_jail = True
-      print("\t\tplayer",self.__player_number,"is now in jail")
+      print("\t\tplayer",self.id,"is now in jail")
       self.jail_counter = 0
 
-   # get_property(contributor : string , property_value : int) : void
-   # sell_property(property : int, reciever : string ) : int
+   # buy_property(deed : deeds) : void
+   def add_property(self,deed):
+      copy_list = []
+      copy_list = copy.deepcopy(self.deeds)
+      copy_list.append(deed)
+      self.deeds = copy.deepcopy(copy_list)
+      copy_list.clear()
+      # self.deeds.append(deed)
+      
+      print("\t\tplayer",self.id,"received",deed.name)
+      
+   # remove_property(property : int, reciever : string ) : # maybe
+   def remove_property(self,index):
+      for i in range(len(self.deeds)):
+         if index == self.deeds[i].index:
+            target_deed = self.deeds[i]
+            self.deeds.pop(i)
+            return target_deed
+         
    # mortgage_property() : void 
    # unmortgage_propety() : void
    # develop_building(property : int) : void
