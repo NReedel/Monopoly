b89dc5c2b18afe24561e7967fa766d33d83781d8
Alicyn <75287008+AlicynRK@users.noreply.github.com>
2023-08-07 19:00:05 -0400

    [66] Implement deck objects in bank class
    Initialized community_chest and chance as deck objects instead of lists (the deck class has a list data type.) Changed card/deck initialization to initialize the deck list with card objects. Added shuffle method call in board constructor to immediately shuffle both decks after initialization.

--- static/py/board.py
+++ static/py/board.py
@@ -2,19 +2,17 @@
 # board.py
 ###############################################################
 from tiles import *
-from board_tiles import *
 from cards import *
+from deck import *
 import json
-# from deeds import Deed ⚠
-# from bank import Bank ⚠
 
 ###############################################################
 class Board:
    
       # --Data--
 	tile = []
-	community_chest = []
-	chance = []
+	community_chest = Deck()
+	chance = Deck()
  
 	# --Constructor--
 	def __init__(self):
@@ -22,21 +20,29 @@ class Board:
 		with open('tiles.json', 'r') as rf:
 			for tiles in json.load(rf):
 				if tiles['type'] == "street":
-					self.tile.append(StreetBoardTiles(tiles))
+					self.tile.append(PropertyStreet(tiles))
 				elif tiles['type'] == "railroad":
-					self.tile.append(RailRoadBoardTiles(tiles))
+					self.tile.append(PropertyRailroad(tiles))
 				elif tiles['type'] == "utility":
-					self.tile.append(UtilityBoardTiles(tiles))
+					self.tile.append(PropertyUtility(tiles))
 				else: #tiles['type'] == "special":
-					self.tile.append(SpecialBoardTiles(tiles))
+					if(tiles['special'] == "card"):
+						self.tile.append(TileCard(tiles))      
+					if(tiles['special'] == "tax"):
+						self.tile.append(TileTax(tiles))
+					if(tiles['special'] == "corner"):
+						self.tile.append(TileCorner(tiles))
 
 		with open('community_chest.json', 'r') as rf:
 			for cards in json.load(rf):
-				self.community_chest.append(Cards(cards))
+				self.community_chest.deck.append(Cards(cards))
 
 		with open('chance.json', 'r') as rf:
 			for cards in json.load(rf):
-				self.chance.append(Cards(cards))
+				self.chance.deck.append(Cards(cards))
+
+		self.chance.shuffle()
+		self.community_chest.shuffle()
     
 	# location(tile_index : int) : str
 	def location(self,tile_index):
