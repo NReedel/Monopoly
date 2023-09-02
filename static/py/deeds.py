2dd46218ad9f1b8eb409f3e52b7025c946d8deaa
NReedel <125296416+NReedel@users.noreply.github.com>
2023-09-02 18:50:43 -0400

    Update deeds.py [124] deeds class current_rent method
    added an Enum class for current_rent based options.
    each subclass recieved its on specified enum class.

--- static/py/deeds.py
+++ static/py/deeds.py
@@ -6,6 +6,7 @@
 # *Purpose: Define Monopoly deed attributes and actions
 ###
 
+from enum import Enum
 
 class Deeds:
     # -- Constructor --
@@ -15,8 +16,18 @@ class Deeds:
         self.price = tile['price']
         self.mortgage_value = tile['deed']['mortgageValue']
         self.unmortgage_value = tile['deed']['unmortgageValue']
-
+    
 class DeedStreet(Deeds):
+
+    class StreetOption(Enum): 
+        R = 0
+        M_R = 1
+        R_H_1 = 2
+        R_H_2 = 3
+        R_H_3 = 4
+        R_H_4 = 5
+        R_H = 6        
+        
     # -- Constructor --
     def __init__(self, tile):
         super().__init__(tile)
@@ -32,25 +43,35 @@ class DeedStreet(Deeds):
 
 
     #def current_rent(self, options_index : int) : int 
-    def current_rent(self, options_index):
-        options = ["r", "m_r", "r_h_1", "r_h_2","r_h_3","r_h_4","r_H"]
-        choice = options[options_index]
-        if(choice == "r"):
+    def current_rent(self, choice):
+        # options = ["r", "m_r", "r_h_1", "r_h_2","r_h_3","r_h_4","r_H"]
+        # choice = options[options_index]
+        # print(choice)
+        # print(self.StreetOption.MR)
+        if(choice == str(self.StreetOption.R)):
             return self.rent
-        if(choice == "m_r"):
+        if(choice == str(self.StreetOption.M_R)):
             return self.monopoly_rent
-        if(choice == "r_h_1"):
+        if(choice == str(self.StreetOption.R_H_1)):
             return self.rent_house_one
-        if(choice == "r_h_2"):
+        if(choice == str(self.StreetOption.R_H_2)):
             return self.rent_house_two
-        if(choice == "r_h_3"):
+        if(choice == str(self.StreetOption.R_H_3)):
             return self.rent_house_three
-        if(choice == "r_h_4"):
+        if(choice == str(self.StreetOption.R_H_4)):
             return self.rent_house_four
-        if(choice == "r_H"):
+        if(choice == str(self.StreetOption.R_H)):
             return self.rent_hotel
+        return 0
 
 class DeedRailroad(Deeds):
+    
+    class RailroadOption(Enum): # new
+        R = 0
+        R_2 = 1 
+        R_3 = 2
+        R_4 = 3
+    
     # -- Constructor --
     def __init__(self, tile):
         super().__init__(tile)
@@ -60,19 +81,24 @@ class DeedRailroad(Deeds):
         self.rent_four = tile['deed']['rent4']
         
     #def current_rent(self, options_index : int) : int 
-    def current_rent(self, options_index):
-        options = ["r", "r_2", "r_3", "r_4"]
-        choice = options[options_index]
-        if(choice == "r"):
+    def current_rent(self, choice):
+        # options = ["r", "r_2", "r_3", "r_4"]
+        # choice = options[options_index]
+        if(choice == str(self.RailroadOption.R)):
             return self.rent
-        if(choice == "r_2"):
+        if(choice == str(self.RailroadOption.R_2)):
             return self.rent_two
-        if(choice == "r_3"):
+        if(choice == str(self.RailroadOption.R_3)):
             return self.rent_three
-        if(choice == "r_4"):
+        if(choice == str(self.RailroadOption.R_4)):
             return self.rent_four
+        return 0
 
 class DeedUtility(Deeds):
+    class UtilityOption(Enum): # new
+        R_M_1 = 0
+        R_M_2 = 1
+        
     # -- Constructor --
     def __init__(self, tile):
         super().__init__(tile)
@@ -80,10 +106,11 @@ class DeedUtility(Deeds):
         self.rent_multiplier_two = tile['deed']['rentMultiplier2']
     
     #def current_rent(self, options_index : int) : int 
-    def current_rent(self, options_index):
-        options = ["r_m_1", "r_m_2"]
-        choice = options[options_index]
-        if(choice == "r_m_1"):
+    def current_rent(self, choice):
+        # options = ["r_m_1", "r_m_2"]
+        # choice = options[options_index]
+        if(choice == str(self.UtilityOption.R_M_1)):
             return self.rent_multiplier_one
-        if(choice == "r_m_2"):
+        if(choice == str(self.UtilityOption.R_M_2)):
             return self.rent_multiplier_two
+        return 0
