28cfec1cb4f6e1b89b0198ce466fc1fbcc136305
Alicyn <75287008+AlicynRK@users.noreply.github.com>
2023-09-03 18:20:26 -0400

    Update cards.py

--- static/py/cards.py
+++ static/py/cards.py
@@ -8,8 +8,9 @@
 # *Purpose: Define Monopoly card attributes and actions
 ###
 
-from asyncio.windows_events import NULL
+# from asyncio.windows_events import NULL
 from enum import Enum
+from card_events import *
 
 ###############################################################
 
@@ -17,7 +18,7 @@ class CardType(Enum):
     CHANCE = 1
     CHEST = 2
     NONE = 3
-
+	
 ###############################################################
 
 class Cards:
@@ -38,29 +39,34 @@ class Cards:
 		self.image = card['image']
 		self.description = card['description']
 
-		# pay a flat amount of money to the bank
+		# card event: pay a flat amount of money to the bank
 		if (card.get('payStaticAmount') != None):
 			self.event = "payStaticAmount"
-			self.pay_money = card[self.event]
-		# collect a flat amount of money from the bank
-		elif (card.get('collectStaticAmount') != None):
-			self.event = "collectStaticAmount"
-			self.collect_money = card[self.event]
-		# pay money to each player
+			self.card_event = PayStaticAmount(self.event, card[self.event])
+
+		# card event: receive a flat amount of money from the bank
+		elif (card.get('receiveStaticAmount') != None):
+			self.event = "receiveStaticAmount"
+			self.card_event = ReceiveStaticAmount(self.event, card[self.event])
+
+		# card event: pay money to each player
 		elif (card.get('payPlayerRateAmount') != None):
 			self.event = "payPlayerRateAmount"
 			self.pay_player_rate = card[self.event]
-		# collect money from each player
-		elif (card.get('collectPlayerRateAmount') != None):
-			self.event = "collectPlayerRateAmount"
-			self.collect_player_rate = card[self.event]
-		# pay money per owned house and hotel to the bank
+
+		# card event: receive money from each player
+		elif (card.get('receivePlayerRateAmount') != None):
+			self.event = "receivePlayerRateAmount"
+			self.receive_player_rate = card[self.event]
+
+		# card event: pay money per owned house and hotel to the bank
 		elif (card.get('payHouseRateAmount') != None and
 			  card.get('payHotelRateAmount') != None):
 			self.event = "payBuildingRateAmount"
 			self.payHouseRate = card['payHouseRateAmount']
 			self.payHotelRate = card['payHotelRateAmount']
-		# move to the specified tile OR move backwards three spaces
+
+		# card event: move to the specified tile OR move backwards three spaces
 		elif (card.get('moveTo') != None):
 			if (card.get('moveTo') >= 0):
 				self.event = "move"
@@ -68,13 +74,15 @@ class Cards:
 			else:
 				self.event = "reverseMove"
 				self.reverse_move = card['moveTo']
-		# move to the nearest specified tile and pay a specified rent rate
+
+		# card event: move to the nearest specified tile and pay a specified rent rate
 		elif (card.get('cardRentMultiplier') != None and
 			  card.get('moveToNearest') != None):
 			self.event = "moveToNearest"
 			self.tile_type = card['moveToNearest']
 			self.card_rent_multiplier = card['cardRentMultiplier']
-		# receive a GOJF card
+
+		# card event: receive a GOJF card
 		elif (card.get('isGOJF') != None):
 			self.is_ownable = card['isGOJF']
 
