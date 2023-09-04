24974e5e31a9bd489189f89fa161a60f22e30592
NReedel <125296416+NReedel@users.noreply.github.com>
2023-09-04 18:25:00 -0400

    Update monopoly.py

--- static/py/monopoly.py
+++ static/py/monopoly.py
@@ -24,27 +24,27 @@ class Monopoly:
       initial_players = int(2)
       monopoly_game = Game()
       main_menu = MainMenuEvents()  
-      #Main Menu Events
+      ####Main Menu Events
       while start_game == False:
          choice = main_menu.display_event_options(initial_players)
-         # main_event.event() uses unpacking to modify data
          start_game, initial_players, exit_menu = main_menu.event(main_menu.events[int(choice)], start_game, initial_players, exit_menu)
          if exit_menu == True:
             del monopoly_game 
+            print("\nExiting...\n")
             return False
       ###End Main Menu Events
       print("\nStarting Game... ")
       monopoly_game.all_players.clear()   
       for i in range(1,initial_players+1): # initialize dynamic players list
          player = Players(monopoly_game.starting_total, str(i))
+         # player.location_name = str(monopoly_game.board.tile[0].tile_name)
          monopoly_game.all_players.append(player)
          starting_pos = monopoly_game.all_players[i-1].current_location()
-         del player
          # print("\tinitializing player",i)
-      
+         del player
+      # monopoly_game.transfer_all()  
       ###Start Game
       print("\nRound ",monopoly_game.round,"\n")
-      
       while end_game == False: # Taking turn
          monopoly_game.take_turn(monopoly_game.all_players)
          if monopoly_game.all_players[monopoly_game.turn-1].bankrupt == True: 
@@ -55,25 +55,25 @@ class Monopoly:
             monopoly_game.all_players[monopoly_game.turn-1].deeds.clear()
             monopoly_game.all_players.pop(monopoly_game.turn-1)
             print("\n\tCurrently",len(monopoly_game.all_players),"player(s) remaining\n")
-            # monopoly_game.turn += 1
             monopoly_game.end_round_check(monopoly_game.all_players)
+            
             if len(monopoly_game.all_players) == 1:
                ###End Game    
                end_game = True
                print("\tPlayer", monopoly_game.all_players[0].id,"wins!\n")
                monopoly_game.all_players[0].deeds.clear()
                print("Game Over\n")
-      del monopoly_game          
+               
+      del monopoly_game
+      input("\tpress enter to continue")  
       return True 
 
    #--Main Executable--
    running = True
    while running == True:
       running = run()
-      if running == True:  
-         input("\tpress enter to continue")
-      else:
-         print("\nExiting...\n")
+
+         
 
 
    
