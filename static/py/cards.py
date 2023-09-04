e427228f7ec4d9cfb24a5bfbc860ae0b9f6aadfc
Alicyn <75287008+AlicynRK@users.noreply.github.com>
2023-09-04 17:38:58 -0400

    Update cards.py

--- static/py/cards.py
+++ static/py/cards.py
@@ -14,6 +14,10 @@ from card_events import *
 
 ###############################################################
 
+# (personal note) all cards have virtually identical attributes, so a hierarchy would be unnecessary. Hence, enums
+# But then why do we need enums here? What purpose do they serve?
+# Maybe these will be used for display purposes?
+# Personal Reminder: event_name is used for most ctor arguments for card events, not just for storing the string. LEAVE IT IN.
 class CardType(Enum):
     CHANCE = 1
     CHEST = 2
@@ -41,50 +45,59 @@ class Cards:
 
 		# card event: pay a flat amount of money to the bank
 		if (card.get('payStaticAmount') != None):
-			self.event = "payStaticAmount"
-			self.card_event = PayStaticAmount(self.event, card[self.event])
+			self.event_name = "payStaticAmount"
+			self.card_event = PayStaticAmount(self.event_name, card[self.event_name])
 
 		# card event: receive a flat amount of money from the bank
 		elif (card.get('receiveStaticAmount') != None):
-			self.event = "receiveStaticAmount"
-			self.card_event = ReceiveStaticAmount(self.event, card[self.event])
+			self.event_name = "receiveStaticAmount"
+			self.card_event = ReceiveStaticAmount(self.event_name, card[self.event_name])
 
 		# card event: pay money to each player
 		elif (card.get('payPlayerRateAmount') != None):
-			self.event = "payPlayerRateAmount"
-			self.pay_player_rate = card[self.event]
+			self.event_name = "payPlayerRateAmount"
+			self.card_event = PayPlayerRateAmount(self.event_name, card[self.event_name])
 
 		# card event: receive money from each player
 		elif (card.get('receivePlayerRateAmount') != None):
-			self.event = "receivePlayerRateAmount"
-			self.receive_player_rate = card[self.event]
+			self.event_name = "receivePlayerRateAmount"
+			self.card_event = ReceivePlayerRateAmount(self.event_name, card[self.event_name])
 
 		# card event: pay money per owned house and hotel to the bank
 		elif (card.get('payHouseRateAmount') != None and
 			  card.get('payHotelRateAmount') != None):
-			self.event = "payBuildingRateAmount"
-			self.payHouseRate = card['payHouseRateAmount']
-			self.payHotelRate = card['payHotelRateAmount']
-
-		# card event: move to the specified tile OR move backwards three spaces
-		elif (card.get('moveTo') != None):
-			if (card.get('moveTo') >= 0):
-				self.event = "move"
-				self.move_to = card['moveTo']
-			else:
-				self.event = "reverseMove"
-				self.reverse_move = card['moveTo']
+			self.event_name = "payBuildingRateAmount"
+			self.card_event = PayBuildingRateAmount(self.event_name, card['payHouseRateAmount'], card['payHotelRateAmount'])
+			
+		# card event: move to the specified tile
+		elif (card.get('moveToIndex') != None):
+			self.event_name = "moveToIndex"
+			self.card_event = MoveToIndex(self.event_name, card[self.event_name])
 
 		# card event: move to the nearest specified tile and pay a specified rent rate
-		elif (card.get('cardRentMultiplier') != None and
-			  card.get('moveToNearest') != None):
-			self.event = "moveToNearest"
-			self.tile_type = card['moveToNearest']
-			self.card_rent_multiplier = card['cardRentMultiplier']
+		elif (card.get('moveToNearest') != None and
+			  card.get('cardRentMultiplier') != None):
+			self.event_name = "moveToNearest"
+			if (card['moveToNearest'] == "Utility"):
+				self.isMoveToUtility = True
+				self.card_event = MoveToNearestUtility(self.event_name, card[self.event_name], card['cardRentMultiplier'])
+			elif (card['moveToNearest'] == "Railroad"):
+				self.isMoveToRailroad = True
+				self.card_event = MoveToNearestRailroad(self.event_name, card[self.event_name], card['cardRentMultiplier'])
+			else:
+				self.isMoveToUtility = False
+				self.isMoveToRailroad = False
+				print("Invalid moveToNearest card type in json; neither Railroad nor Utility")
 
-		# card event: receive a GOJF card
+		# card event: move the specified number of tiles (forward or backward)
+		elif (card.get('moveSpaces') != None):
+			self.event_name = "moveSpaces"
+			self.card_event = MoveSpaces(self.event_name, card[self.event_name])
+			
+		# card event: receive a Get Out of Jail Free (GOJF) card
 		elif (card.get('isGOJF') != None):
-			self.is_ownable = card['isGOJF']
+			self.event_name = "isGOJF"
+			self.card_event = GetOutOfJailFree(self.event_name, card[self.event_name])
 
 
 	def render_html():
