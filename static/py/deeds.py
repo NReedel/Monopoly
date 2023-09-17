79cd0be35c25303c8dc1e62199759063c50b5ae5
NReedel <125296416+NReedel@users.noreply.github.com>
2023-09-17 15:08:13 -0400

    Update deeds.py

--- static/py/deeds.py
+++ static/py/deeds.py
@@ -11,7 +11,6 @@ from enum import Enum
 class Deeds:
     # -- Constructor --
     def __init__(self, tile):
-
         self.index = tile['index']
         self.name = tile['name']
         self.price = tile['price']
@@ -115,44 +114,3 @@ class DeedUtility(Deeds):
         if(choice == self.UtilityOption.R_M_2.value):
             return self.rent_multiplier_two
         return 0
-=======
-        self.index = tile.index
-        self.name = tile.name
-        self.price = tile.price
-        self.mortgage_value = tile.deed.mortgageValue
-        self.unmortgage_value = tile.deed.unmortgageValue
-
-
-class DeedStreet(Deeds):
-    # -- Constructor --
-    def __init__(self, tile):
-        super().__init__(tile)
-        self.rent = tile.deed.rent
-        self.monopoly_rent = tile.deed.monopolyRent
-        self.rent_house_one = tile.deed.rentHouse1
-        self.rent_house_two = tile.deed.rentHouse2
-        self.rent_house_three = tile.deed.rentHouse3
-        self.rent_house_four = tile.deed.rentHouse4
-        self.rent_hotel = tile.deed.rentHotel
-        self.house_cost = tile.deed.houseCost
-        self.hotel_cost = tile.deed.hotelCost
-
-
-class DeedRailroad(Deeds):
-    # -- Constructor --
-    def __init__(self, tile):
-        super().__init__(tile)
-        self.rent = tile.deed.rent
-        self.rent_two = tile.deed.rent2
-        self.rent_three = tile.deed.rent3
-        self.rent_four = tile.deed.rent4
-
-
-class DeedUtility(Deeds):
-    # -- Constructor --
-    def __init__(self, tile):
-        super().__init__(tile)
-        self.rent_multiplier_one = 4    # These can be changed later if they become attributes from file
-        self.rent_multiplier_two = 10
-
-
