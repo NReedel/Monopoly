bf8895629d57715321e3f6fc9661818662732900
NReedel <125296416+NReedel@users.noreply.github.com>
2023-09-21 22:08:56 -0400

    Update game.py [51] add sort for deeds in player and bankclass
    added Player.sort_deeds to end of transfer_deed method, using a conditional.

--- static/py/game.py
+++ static/py/game.py
@@ -256,7 +256,10 @@ class Game:
       print("\t\t\""+str(target_deed.name)+"\"","received\n")
       self.board.tile[location].owned_by = recipient.id
       self.board.tile_check(self.all_players)
-
+      if owner.id != "bank":
+         owner.sort_deeds()
+      if recipient.id != "bank":   
+         recipient.sort_deeds()
       
    # jailed_move_attempt(self, player : Players) : void
    def jailed_move_attempt(self,player): 
